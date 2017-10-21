651a422a89c6120781ee58d40eb06f9aa29d4610
Nicholas Hoang <nickhoang04@gmail.com>
2017-10-21 10:14:43 -0700

    Initial Commit

---
+++ swift-practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-practice
+//
+//  Created by UCode on 10/21/17.
+//  Copyright © 2017 Nick Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
