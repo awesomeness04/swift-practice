2c96fa6eb3e16027cd5df84da0901b31fd9ac69d
Nicholas Hoang <nickhoang04@gmail.com>
2017-10-28 11:59:25 -0700

    5 question, multiple choice, random trivia quiz........with score.

--- swift-practice/main.swift
+++ swift-practice/main.swift
@@ -1,8 +1,10 @@
+import Foundation
+
 //let apples = 5
 //let appleCounter = "You have \(apples) apples!"
 //print(appleCounter)
 
-var userApples: Int? = 9
+//var userApples: Int? = 9
 //if apples !=  nil {
 //    print("You have \(apples!) Apple products in your online shopping cart!" )
 //}
@@ -24,15 +26,104 @@ var userApples: Int? = 9
 
 
 
-print("What's your name?")
+//print("What's your name?")
+//
+//let name = readLine()
+//if let nameTwo = name {
+//    print("Hello, \(nameTwo)!")
+//}
+//else {
+//    print("Hello, person who has no name!")
+//}
 
-let name = readLine()
-if let nameTwo = name {
-    print("Hello, \(nameTwo)!")
-}
-else {
-    print("Hello, person who has no name!")
-}
+//print("Enter a number.")
+//let firstNum = readLine()
+//print("Enter a second number.")
+//let secondNum = readLine()
+//
+//guard let unwrappedFirstNum = firstNum
+//    else {
+//        print("YOU, SHALL NOT PASS!!!")
+//        exit(1)
+//}
+//guard let unwrappedSecondNum = secondNum
+//    else {
+//        print("YOU, SHALL NOT PASS!!!")
+//        exit(1)
+//}
+//
+//
+//print("The sum of the two numbers is \(Double(unwrappedFirstNum)! + Double(unwrappedSecondNum)!)")
 
+//Q:23rd President
+//A:BENJAMIN HARRISON, William McKinley, Grover Cleveland, Chester Arthur
 
+//Q:Which city is NOT a capital of a state?
+//A:Montepelier, Harrisburg, Helena, CHARLOTTE
 
+//Q:Which of these quotes is NOT an ancient proverb?
+//A:"Fall down 7 times, get back up 8", "MONEY OFTEN COSTS TOO MUCH", "To have joy one must share it. Happiness was born a twin", "Give a man a fish and you feed him for a day. Teach a man to fish and you feed him for a lifetime."
+
+//Q:Population of New York City?
+//A: 8.5 MILLION, 7.4 million, 9.1 million, 9.2 million
+
+//Q:Which city is not real?
+//A:BIGFOOT, IOWA, Embarass, Minnesota, Dogtown, Alabama, Chicken, Alaska
+
+//var score = 0
+//print("Hello! You are taking a quiz! But first, what's your name?")
+//let name = readLine()
+//print("Q:23rd President?\n\tA.Benjamin Harrison\n\tB.William McKinley\n\tC.Grover Cleveland\n\tD.Chester Arthur")
+//let one = readLine()
+//if one == "A" {
+//    score = score + 1
+//    print("Good job! You get a point! Now you have \(score) point!")
+//}
+//else {
+//    print("Aww....incorrect. Nice try, though! You have \(score) points so far.")
+//}
+//print("Q:Which city is NOT a capital of a state?\n\tA.Montepelier\n\tB.Harrisburg\n\tC.Helena\n\tD.Charlotte")
+//let two = readLine()
+//if two == "D" {
+//    score = score + 1
+//    print("Nice work! You get a point! Now you have \(score) points!")
+//}
+//else {
+//    print("Aww....you're wrong. Nice try, though! You have \(score) points so far.")
+//}
+//print("Q:Which of these quotes is NOT an ancient proverb?\n\tA.Fall down 7 times, get back up 8\n\tB.Money often costs too much\n\tC.To have joy one must share it. Happiness was born a twin\n\tD.Give a man a fish and you feed him for a day. Teach a man to fish and you feed him for a lifetime")
+//let three = readLine()
+//if three == "B" {
+//    score = score + 1
+//    print("Splendid! You get a point! Now you have \(score) points!")
+//}
+//else {
+//    print("Aww....you got that one wrong. Nice try, though! You have \(score) points so far.")
+//}
+//print("Q:Population of New York City?\n\tA.8.5 million\n\tB.7.4 million\n\tC.9.1 million\n\tD.9.2 million")
+//let four = readLine()
+//if four == "A" {
+//    score = score + 1
+//    print("Amazing! You get a point! Now you have \(score) points!")
+//}
+//else {
+//    print("Aww....you failed to answer the question appropiately. Nice try, though! You have \(score) points so far.")
+//}
+//print("Q:Which city does not exist?\n\tA.Bigfoot, Iowa\n\tB.Embarrass, Minnesota\n\tC.Dogtown, Alabama\n\tD.Chicken, Alaska")
+//let five = readLine()
+//if five == "A" {
+//    score = score + 1
+//    print("Wowza! You get a point! You finished with \(score) points!")
+//}
+//else {
+//    print("Aww....you failed... Nice try, though! You finished with \(score) points.")
+//}
+
+//let coordinate = (x:3,y:2)
+//print(coordinate.x)
+
+//var emptyTuple: (x: Int, y: Int)
+
+let integerArray = [1,2,3,4,5]
+let emptyArray : [String]
+//print(emptyArray)
